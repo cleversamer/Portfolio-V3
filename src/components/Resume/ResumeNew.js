4f109831896a0ad146a2e1ace780aef32f37472a
cleversamerr <thedev.samer@gmail.com>
2023-03-24 10:46:36 +0200

    fixed bugs

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -27,7 +26,7 @@ const ResumeNew = () => {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            // href={pdf}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
@@ -45,7 +44,7 @@ const ResumeNew = () => {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            // href={pdf}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
